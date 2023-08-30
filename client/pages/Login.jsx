0b48367fd85b85acaf8c73a759a0e7a86311bbe2
Andrea-Teres <ateresprieto@gmail.com>
2023-08-30 19:45:45 +0200

    Login page styling

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,19 +1,46 @@
 import React from "react";
+import "/styles/LoginSignup.css";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 export default function Login() {
   return (
     <div>
-      <div>
-        <h2>Log in</h2>
-        <div>
-          <input placeholder="Email"></input>
-          <input placeholder="Password"></input>
-        </div>
-        <button>Log in</button>
-      </div>
-      <div>
-        <p>Not a member? Sign up here!</p>
-      </div>
+      <Container maxWidth="md" align="center">
+        <h1>Log in</h1>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 2, width: "30ch" },
+            mt: 2,
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              color="secondary"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              color="secondary"
+            />
+          </div>
+        </Box>
+        <Button variant="outlined" color="secondary">
+          Log in
+        </Button>
+        <Box sx={{ mt: 2 }}>
+          <p>Not a member? Sign up here!</p>
+        </Box>
+      </Container>
 
       {/* Link to Sign up page */}
     </div>
