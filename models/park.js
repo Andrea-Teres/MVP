582b53158d5008fc7c43dedb1ac894c2b3765cbc
Andrea-Teres <ateresprieto@gmail.com>
2023-09-01 18:49:37 +0200

    sequelizing parks endpoints

--- models/park.js
+++ models/park.js
@@ -26,5 +26,5 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "Park",
     }
   );
-  return Park;
+  return Parks;
 };
---
+++ routes/parks.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const models = require("../models");
+const { Sequelize } = require("sequelize");
+
+//POST park into wishlist DB
+//the park must exist
+//the park shouldn't already be in the DB
+
+//GET all items from wishlist
+
+// REMOVE one item from wishlist
