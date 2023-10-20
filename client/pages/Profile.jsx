1081b43d5983d2939cfa3c92ba5900eb56faf9ee
Andrea-Teres <ateresprieto@gmail.com>
2023-10-20 14:44:27 +0200

    styling profile page

--- client/pages/Profile.jsx
+++ client/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import AttractionsOutlinedIcon from "@mui/icons-material/AttractionsOutlined";
 import NavBar from "../components/NavBar";
-import { Typography, Container } from "@mui/material";
+import { Typography, Container, IconButton } from "@mui/material";
 
 export default function Profile() {
   return (
@@ -9,6 +9,7 @@ export default function Profile() {
 
       <Container display="flex" maxWidth="lg" align="left">
         <Typography variant="h3" sx={{ mt: 5, mb: 2 }}>
+          <AttractionsOutlinedIcon fontSize="inherit" sx={{ mb: 2, mr: 1 }} />
           My Profile
         </Typography>
       </Container>
